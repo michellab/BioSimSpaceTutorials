06ea811b28290bab053930fc22ff4a8cd1c1fa20
jmichel1980 <julien.michel@ed.ac.uk>
2021-04-18 22:55:56 +0000

    first version of master SLURM bash script for processing an FEP dataset on plato

---
+++ 03_fep/execution_model/scripts/BSSrunFEP.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo $SLURM_JOB_NAME
+echo $SLURM_ARRAY_TASK_ID
+date
+/home/users/common/BioSimSpace/sire.app/bin/python scripts/BSSrunFEP.py $1 $2 $3 $SLURM_ARRAY_TASK_ID
+sleep 5
+exit 0
