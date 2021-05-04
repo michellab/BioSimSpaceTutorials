70eac561a87aef260d6173def5b1753f4e9c50b4
Lester Hedges <lester.hedges@gmail.com>
2021-05-04 21:41:28 +0100

    Added example workflow script.

---
+++ 01_introduction/scripts/workflow.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Exit immediately on error.
+set -e
+
+echo "Parameterising..."
+python nodes/parameterise.py --pdb inputs/methanol.pdb --forcefield gaff
+
+echo "Solvating..."
+python nodes/solvate.py --files parameterised.* --water_model tip3p
+
+echo "Minimising..."
+python nodes/minimise.py --files solvated.* --steps 1000
+
+echo "Equilibrating..."
+python nodes/equilibrate.py --files minimised.* --restraint heavy
+
+echo "Done!"
